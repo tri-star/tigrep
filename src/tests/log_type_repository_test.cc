5c7ec7d2fc49ab2bf8911567c07711375fe3062f
tristar <tristar@urban-theory.net>
2014-08-21 07:20:34 +0900

    Add LogEntryRepository and its test.

---
+++ src/tests/log_type_repository_test.cc
@@ -0,0 +1,35 @@
+#include "../log_type_repository.h"
+
+#include <gtest/gtest.h>
+
+namespace tigrep {
+
+  TEST(LogTypeRepositoryTest, basic) {
+    LogTypeRepository repository;
+    LogTypeEntry entry1, entry2;
+
+    entry1.name = "aaa";
+    entry1.regex = "regex1";
+    entry1.format = "format1";
+
+    entry2.name = "bbb";
+    entry2.regex = "regex2";
+    entry2.format = "format2";
+
+    repository.addEntry(entry1);
+    repository.addEntry(entry2);
+
+    LogTypeEntry* result = repository.getEntry("aaa");
+
+    ASSERT_TRUE(result != NULL);
+    ASSERT_STREQ("regex1", result->regex.c_str());
+
+    result = repository.getEntry("bbb");
+    ASSERT_TRUE(result != NULL);
+    ASSERT_STREQ("regex2", result->regex.c_str());
+
+    result = repository.getEntry("bbc");
+    ASSERT_TRUE(result == NULL);
+  }
+
+} //name space
